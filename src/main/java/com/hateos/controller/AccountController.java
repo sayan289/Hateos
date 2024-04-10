14df5f7f2755e97848f6719f161986e3b05754c8
sayan289 <sayanadhikary289@gmail.com>
2024-04-10 12:33:54 +0530

    Adding test api

--- src/main/java/com/hateos/controller/AccountController.java
+++ src/main/java/com/hateos/controller/AccountController.java
@@ -96,6 +96,6 @@ public class AccountController {
     @GetMapping("/test")
     public String test()
     {
-        return "Welcome to jenkins jenkins";
+        return "Welcome to jenkins";
     }
 }
